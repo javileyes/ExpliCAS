fe029730471660d993c6e49bd7b0ce28dda263c1
Javier Giménez Moya <javiergimenezmoya@gmail.com>
2025-11-28 14:09:49 +0100

    refactoring del Simplifier

--- crates/cas_engine/src/engine.rs
+++ crates/cas_engine/src/engine.rs
@@ -95,179 +95,12 @@ impl Simplifier {
     }
 
     pub fn simplify(&self, expr: Rc<Expr>) -> (Rc<Expr>, Vec<Step>) {
-        self.simplify_rec(expr)
-    }
-
-    fn simplify_rec(&self, expr: Rc<Expr>) -> (Rc<Expr>, Vec<Step>) {
-        let mut steps = Vec::new();
-
-        // 1. Simplify children first (Bottom-Up)
-        let (mut current_expr, child_steps) = self.simplify_children(expr);
-        steps.extend(child_steps);
-
-        // 2. Apply rules to the current node
-        // We loop here to handle multiple rule applications on the same node level 
-        // before potentially recursing if the structure changes significantly?
-        // Actually, if we recurse on change, we don't need a loop here.
-        // But to avoid deep recursion for simple chains (A->B->C), a loop is better.
-        
-        let mut changed = true;
-        while changed {
-            changed = false;
-            let variant = get_variant_name(&current_expr);
-            
-            // Try specific rules
-            if let Some(specific_rules) = self.rules.get(variant) {
-                for rule in specific_rules {
-                    if let Some(rewrite) = rule.apply(&current_expr) {
-                        if self.collect_steps {
-                            steps.push(Step::new(
-                                &rewrite.description,
-                                rule.name(),
-                                current_expr.clone(),
-                                rewrite.new_expr.clone(),
-                            ));
-                        }
-                        current_expr = rewrite.new_expr;
-                        changed = true;
-                        break; // Restart loop with new expression
-                    }
-                }
-            }
-            
-            if changed { continue; }
-
-            // Try global rules
-            for rule in &self.global_rules {
-                if let Some(rewrite) = rule.apply(&current_expr) {
-                    if self.collect_steps {
-                        steps.push(Step::new(
-                            &rewrite.description,
-                            rule.name(),
-                            current_expr.clone(),
-                            rewrite.new_expr.clone(),
-                        ));
-                    }
-                    current_expr = rewrite.new_expr;
-                    changed = true;
-                    break; // Restart loop
-                }
-            }
-            
-            // If we changed, we might need to re-simplify children if the structure changed?
-            // If A -> B, and B has children, are they simplified?
-            // If the rule just modified the top level (e.g. x+x -> 2x), children are same (x).
-            // If the rule introduced new structure (e.g. (x+1)^2 -> x^2+2x+1), the new children (x^2, 2x, 1) are likely simplified or simple.
-            // But strictly speaking, we should recurse.
-            // However, full recursion might be expensive.
-            // Let's stick to the loop for now. If a rule produces something unsimplified, 
-            // the next iteration of the loop will see it as the current node.
-            // But if it produces a tree, we only look at the root of that tree.
-            // Correctness requires checking children of the result.
-            // So: if changed, we should probably call simplify_rec on the result?
-            // But that risks infinite recursion if a rule cycles.
-            // The original code restarted the loop from the top.
-            // Here we are at a specific node.
-            
-            if changed {
-                // To be safe and correct (Bottom-Up), if we change the node, we should treat it as a new subtree.
-                // So we should recurse.
-                let (new_expr_rec, new_steps_rec) = self.simplify_rec(current_expr);
-                steps.extend(new_steps_rec);
-                return (new_expr_rec, steps);
-            }
-        }
-
-        (current_expr, steps)
-    }
-
-    fn simplify_children(&self, expr: Rc<Expr>) -> (Rc<Expr>, Vec<Step>) {
-        let mut steps = Vec::new();
-        let new_expr = match expr.as_ref() {
-            Expr::Add(l, r) => {
-                let (new_l, l_steps) = self.simplify_rec(l.clone());
-                let (new_r, r_steps) = self.simplify_rec(r.clone());
-                steps.extend(l_steps);
-                steps.extend(r_steps);
-                if new_l != *l || new_r != *r {
-                    Expr::add(new_l, new_r)
-                } else {
-                    expr.clone()
-                }
-            }
-            Expr::Sub(l, r) => {
-                let (new_l, l_steps) = self.simplify_rec(l.clone());
-                let (new_r, r_steps) = self.simplify_rec(r.clone());
-                steps.extend(l_steps);
-                steps.extend(r_steps);
-                if new_l != *l || new_r != *r {
-                    Expr::sub(new_l, new_r)
-                } else {
-                    expr.clone()
-                }
-            }
-            Expr::Mul(l, r) => {
-                let (new_l, l_steps) = self.simplify_rec(l.clone());
-                let (new_r, r_steps) = self.simplify_rec(r.clone());
-                steps.extend(l_steps);
-                steps.extend(r_steps);
-                if new_l != *l || new_r != *r {
-                    Expr::mul(new_l, new_r)
-                } else {
-                    expr.clone()
-                }
-            }
-            Expr::Div(l, r) => {
-                let (new_l, l_steps) = self.simplify_rec(l.clone());
-                let (new_r, r_steps) = self.simplify_rec(r.clone());
-                steps.extend(l_steps);
-                steps.extend(r_steps);
-                if new_l != *l || new_r != *r {
-                    Expr::div(new_l, new_r)
-                } else {
-                    expr.clone()
-                }
-            }
-            Expr::Pow(b, e) => {
-                let (new_b, b_steps) = self.simplify_rec(b.clone());
-                let (new_e, e_steps) = self.simplify_rec(e.clone());
-                steps.extend(b_steps);
-                steps.extend(e_steps);
-                if new_b != *b || new_e != *e {
-                    Expr::pow(new_b, new_e)
-                } else {
-                    expr.clone()
-                }
-            }
-            Expr::Neg(e) => {
-                let (new_e, e_steps) = self.simplify_rec(e.clone());
-                steps.extend(e_steps);
-                if new_e != *e {
-                    Expr::neg(new_e)
-                } else {
-                    expr.clone()
-                }
-            }
-            Expr::Function(name, args) => {
-                let mut new_args = Vec::new();
-                let mut args_changed = false;
-                for arg in args {
-                    let (new_arg, arg_steps) = self.simplify_rec(arg.clone());
-                    if new_arg != *arg {
-                        args_changed = true;
-                    }
-                    steps.extend(arg_steps);
-                    new_args.push(new_arg);
-                }
-                if args_changed {
-                    Rc::new(Expr::Function(name.clone(), new_args))
-                } else {
-                    expr.clone()
-                }
-            }
-            _ => expr.clone(),
+        let mut transformer = SimplificationTransformer {
+            simplifier: self,
+            steps: Vec::new(),
         };
-        (new_expr, steps)
+        let new_expr = transformer.transform_expr(expr);
+        (new_expr, transformer.steps)
     }
 
     pub fn are_equivalent(&self, a: Rc<Expr>, b: Rc<Expr>) -> bool {
@@ -310,6 +143,92 @@ impl Simplifier {
         let mut collector = VariableCollector::new();
         collector.visit_expr(expr);
         collector.vars
+    }
+}
+
+struct SimplificationTransformer<'a> {
+    simplifier: &'a Simplifier,
+    steps: Vec<Step>,
+}
+
+use cas_ast::visitor::Transformer;
+
+impl<'a> Transformer for SimplificationTransformer<'a> {
+    fn transform_expr(&mut self, expr: Rc<Expr>) -> Rc<Expr> {
+        // 1. Simplify children first (Bottom-Up)
+        let expr_with_simplified_children = match expr.as_ref() {
+            Expr::Number(_) => self.transform_number(expr.clone()),
+            Expr::Constant(_) => self.transform_constant(expr.clone()),
+            Expr::Variable(_) => self.transform_variable(expr.clone()),
+            Expr::Add(l, r) => self.transform_add(expr.clone(), l, r),
+            Expr::Sub(l, r) => self.transform_sub(expr.clone(), l, r),
+            Expr::Mul(l, r) => self.transform_mul(expr.clone(), l, r),
+            Expr::Div(l, r) => self.transform_div(expr.clone(), l, r),
+            Expr::Pow(b, e) => self.transform_pow(expr.clone(), b, e),
+            Expr::Neg(e) => self.transform_neg(expr.clone(), e),
+            Expr::Function(name, args) => self.transform_function(expr.clone(), name, args),
+        };
+
+        // 2. Apply rules to the current node
+        self.apply_rules(expr_with_simplified_children)
+    }
+}
+
+impl<'a> SimplificationTransformer<'a> {
+    fn apply_rules(&mut self, mut expr: Rc<Expr>) -> Rc<Expr> {
+        loop {
+            let mut changed = false;
+            let variant = get_variant_name(&expr);
+            
+            // Try specific rules
+            if let Some(specific_rules) = self.simplifier.rules.get(variant) {
+                for rule in specific_rules {
+                    if let Some(rewrite) = rule.apply(&expr) {
+                        if self.simplifier.collect_steps {
+                            self.steps.push(Step::new(
+                                &rewrite.description,
+                                rule.name(),
+                                expr.clone(),
+                                rewrite.new_expr.clone(),
+                            ));
+                        }
+                        expr = rewrite.new_expr;
+                        changed = true;
+                        break; // Restart loop with new expression
+                    }
+                }
+            }
+            
+            if changed { 
+                // If changed, re-simplify the whole tree
+                return self.transform_expr(expr);
+            }
+
+            // Try global rules
+            for rule in &self.simplifier.global_rules {
+                if let Some(rewrite) = rule.apply(&expr) {
+                    if self.simplifier.collect_steps {
+                        self.steps.push(Step::new(
+                            &rewrite.description,
+                            rule.name(),
+                            expr.clone(),
+                            rewrite.new_expr.clone(),
+                        ));
+                    }
+                    expr = rewrite.new_expr;
+                    changed = true;
+                    break; // Restart loop
+                }
+            }
+            
+            if changed {
+                // If changed, re-simplify the whole tree
+                return self.transform_expr(expr);
+            }
+
+            // If no rules applied, we are done with this node
+            return expr;
+        }
     }
 }
 
